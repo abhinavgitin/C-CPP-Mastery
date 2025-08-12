27fd97037718831dbf1662008a360a367e97fce2
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-08-12 22:17:25 +0530

    solve one question

--- firstCProgram.c
+++ firstCProgram.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(){
     printf("Hello world \n");
+    int sum = add(5, 10);
+    printf("The sum is: %d\n", sum);
     return 0;
+}
+int add(int a, int b){
+    return a + b;
 }
 // this is the my first C program ever
