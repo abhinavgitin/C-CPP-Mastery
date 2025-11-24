06adfbf887335e376f70d599821a9d58286a274a
abhinavgitin <prabhudarsanpuri987@gmail.com>
2025-11-24 22:08:09 +0530

    add a c program to test extention

---
+++ src/CPProgram.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int i = 10;
+    int j = 20;
+    if ( i < j ){
+        printf(" %d is less than %d\n", i, j);
+    } else {
+        printf(" %d is not less than %d\n", i, j);
+    }
+    return 0;
+}
